a953b2fa802074d7187fab857abb5df22beef8c0
Unseenfaith <1832323+UnseenFaith@users.noreply.github.com>
2023-10-21 14:35:26 -0400

    Working enrage reader

--- src/scripts/EnrageUIReader.ts
+++ src/scripts/EnrageUIReader.ts
@@ -1,42 +1,40 @@
-import { webpackImages, PointLike, RectLike, ImgRef, captureHoldFullRs, Rect } from "alt1";
+import { webpackImages, PointLike, RectLike, ImgRef, captureHoldFullRs, Rect, captureHold, mixColor } from "alt1";
 import * as OCR from "alt1/ocr";
 
-import * as font from "alt1/fonts/aa_8px_mono.js"
-
 let imgs = webpackImages({
-  bottom_right: require("../images/enrage_bottom_right.data.png"),
-  top_left: require("../images/enrage_top_left.data.png"),
-  percent: require("../images/enrage_percent.data.png")
-})
+  enrage: require("../images/enrage.data.png")
+});
 
-export type ModalUI = {
-	rect: RectLike,
-	title: string,
-	img: ImgRef
+export type EnragePosition = {
+	x: number,
+  y: number,
+  w: number,
+  h: number,
+  xos: number,
+  yos: number
 }
 
 export namespace EnrageUIReader {
 	export function find(img?: ImgRef) {
 		if (!img) { img = captureHoldFullRs(); }
-    let top = img.findSubimage(imgs.top_left)[0];
-    if (!top) { return null; }
-    let right = img.findSubimage(imgs.bottom_right)[0];
-    if (!right) { return null; }
-    let percent = img.findSubimage(imgs.percent, top.x, top.y, right.x - top.x, right.y - top.y)[0];
-    if (!percent) { return null }
-
-    let pixels = img.toData(top.x, top.y, right.x - top.x, right.y - top.y)
-
-    let pos = OCR.findChar(pixels, font, [255, 255, 255], 0, 0, pixels.width, pixels.height)
-    console.log(pos)
-		//let enrage = OCR.readChar(pixels, font, [255, 255, 255], percent.x - 7, percent.y + 6, false);
-
-    //console.log(enrage)
-
+    let enrage = img.findSubimage(imgs.enrage)
+    console.log(enrage)
+    if (enrage.length == 0) return null;
     return {
-      rect: new Rect(top.x, top.y, right.x - top.x, right.y - top.y),
-      title: "",
-      img: img
-    } as ModalUI;
+      x: enrage[1].x + 5,
+      y: enrage[1].y + 1,
+      w: 100,
+      h: 40,
+      xos: 34,
+      yos: 12
+    } as EnragePosition;
 	}
+
+  export function read(pos: EnragePosition) {
+    let img = captureHold(pos.x, pos.y, pos.w, pos.h)
+    // alt1.overLayRect(mixColor(0, 255, 0), pos.x, pos.y, pos.w, pos.h, 2500, 1)
+    let str = alt1.bindReadColorString(img.handle, "chat", mixColor(255, 255, 255), pos.xos, pos.yos)
+    str = str.slice(0, str.length - 1);
+    return parseInt(str)
+  }
 }
